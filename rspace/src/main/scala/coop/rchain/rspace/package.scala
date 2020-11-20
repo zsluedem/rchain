5624da458faf98602edf72c8ba7a00bd36ba8262
Will Qiu <zsluedem06@gmail.com>
2020-11-20 15:55:12 +0800

    add HistoryRepositorySyntax

--- rspace/src/main/scala/coop/rchain/rspace/package.scala
+++ rspace/src/main/scala/coop/rchain/rspace/package.scala
@@ -1,7 +1,11 @@
 package coop.rchain
 
 import coop.rchain.metrics.Metrics
+import coop.rchain.rspace.history.syntax.HistoryRepositorySyntax
 
 package object rspace {
   val RSpaceMetricsSource: Metrics.Source = Metrics.Source(Metrics.BaseSource, "rspace")
+  object syntax extends AllSyntaxRSpaceHistory
+
 }
+trait AllSyntaxRSpaceHistory extends HistoryRepositorySyntax
